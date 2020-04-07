4c89af75ec1a86574d8b255e1c12c06711ce7d02
JustAPie <dotranthaian2006@gmail.com>
2020-04-07 13:48:03 +0700

    Lol

--- commands/fun/trietly.js
+++ commands/fun/trietly.js
@@ -15,6 +15,10 @@ module.exports = {
             return message.reply("❌ Please tag an user!")
                 .then(m => m.delete({timeout: 5000}));
         } else {
+            const botUser = message.guild.members.cache.find(m => m.id === "686851924771078154");
+            if (botUser) {
+                return message.reply('Có con cặc!');
+            }
         	return message.channel.send(`${member} Không làm mà đòi có ăn thì chỉ có ăn đầu buồi ăn cứt`);
         }
        
